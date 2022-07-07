b6224d259904114863f1338eec76734d64a0ade9
Rafael Peroco <rafaelperoco@gmail.com>
2022-07-07 01:06:00 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # gitlab-artifacts-cleaner
+
 Golang CLI to delete GitLab stored artifacts
+
+## TL;DR
+
+When you need to wipe out a Gitlab project's artifacts, this is the tool for you. It will delete all the artifacts in a project. It will not delete the project itself. It will not delete the project's repository.
+
+Because of GitLab API limitations, you need to know how many job pages you need to iterate through to delete all the artifacts. This is how GitLab API works. If you don't know it, just use the standard values:
+  
+  ```yaml
+  per_page: 100
+  page: 20
+  ```
+
+## Usage
+
+Parameters:
+
+- `--server`: GitLab server URL. Default: `https://gitlab.com`
+- `--token`: Your GitLab personal access token
+- `--project`: Project ID or path.
+- `--per-page`: Number of jobs to fetch per page. Default: `100`
+- `--page`: Page number. Default: `1`
+
+```bash
+artifacts-cleaner --server https://gitlab.com --token <token> --project_id <project_id> --pages 10 --per_page 100
+```
+
+This is a work in progress. If you have any suggestions, please open an issue.
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+)
+
+type Job struct {
+	ID          int    `json:"id"`
+}
+
+func main () {
+	var project_id int
+	var per_page int
+	var pages int
+	var token string
+	var server string
+
+	flag.IntVar(&project_id, "project_id", 0, "Project ID")
+	flag.IntVar(&per_page, "per_page", 100, "Number of jobs per page")
+	flag.IntVar(&pages, "pages", 1, "Number of pages")
+	flag.StringVar(&token, "token", "", "Private token")
+	flag.StringVar(&server, "server", "", "Gitlab server")
+	flag.Parse()
+
+	fmt.Printf("Fetching %d pages from %v\n", pages, server)
+
+	for i := 1; i <= pages; i++ {
+		url := fmt.Sprintf("%v/api/v4/projects/%v/jobs?per_page=%d&page=%d&artifact_expired=false", server, project_id, per_page, i)
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		println("page number:", i)
+		req.Header.Set("PRIVATE-TOKEN", token)
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer resp.Body.Close()
+
+		var jobs []Job
+		err = json.NewDecoder(resp.Body).Decode(&jobs)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		for _, job := range jobs {
+			fmt.Println("job processed:", job.ID)
+		}
+
+		for _, job := range jobs {
+			url := fmt.Sprintf("%v/api/v4/projects/%v/jobs/%d/artifacts", server, project_id, job.ID)
+			req, err := http.NewRequest("DELETE", url, nil)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			req.Header.Set("PRIVATE-TOKEN", token)
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer resp.Body.Close()
+			fmt.Println("job artifacts deleted:", job.ID)
+		}
+	}
+}
